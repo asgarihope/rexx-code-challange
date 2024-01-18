65b2bacea869f08d27fb85bdc17fa6d05e1cf80a
mac-omid <omidevil200@gmail.com>
2024-01-18 12:03:22 +0330

    [FEATURE] add report route

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require_once 'core/Router.php';
 require_once 'core/Database.php';
 require_once 'src/Controller/HomeController.php';
 require_once 'src/Controller/FileUploadController.php';
+require_once 'src/Controller/ReportController.php';
 
 $db = (new Database())->connect();
 
@@ -16,11 +17,11 @@ $router = new Router();
 
 $router->addRoute('', HomeController::class, 'index');
 $router->addRoute('upload-json', FileUploadController::class, 'uploadJson');
-
 $router->addRoute('about', HomeController::class, 'about');
+$router->addRoute('report', ReportController::class, 'report');
 
 
 $uri = trim($_SERVER['REQUEST_URI'], '/');
 $uri = trim($_SERVER['REQUEST_URI'], '/');
 
-$router->route($uri, $db);
+$router->route(explode('?',$uri)[0], $db);
