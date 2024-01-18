67bcdbc2c35644189ef379568032d5a34c01c9e3
mac-omid <omidevil200@gmail.com>
2024-01-18 12:03:47 +0330

    [FEATURE] separating the config information

---
+++ core/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace core;
+
+class Config
+{
+    const DB_HOST = 'localhost';
+    const DB_USER = 'root';
+    const DB_PASSWORD = '123456789';
+    const DB_NAME = 'rexx';
+    const DEFAULT_LIMIT_REPORT = 2;
+}
